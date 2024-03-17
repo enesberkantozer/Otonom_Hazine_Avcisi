1e592f297a6d7893a8b5e97e386821bb3560e6a1
Enes Berkant Özer <enesberkantozer260@gmail.com>
2024-03-17 15:39:26 +0300

    Revert "Update SetSizePanel.java"
    This reverts commit cb6c3aee846270dc4e962daeb40f38f39228c470.

--- src/myPackage/SetSizePanel.java
+++ src/myPackage/SetSizePanel.java
@@ -23,7 +23,6 @@ public class SetSizePanel extends JPanel implements KeyListener {
 	private static final long serialVersionUID = 1L;
 
 	public static AltinAvcisi oyun;
-	private GameDialog dialog;
 	private JLabel lblCaution;
 	private JTextField textFieldSizeWidth;
 	private JTextField textFieldSizeHeight;
@@ -222,7 +221,7 @@ public class SetSizePanel extends JPanel implements KeyListener {
 	public static JScrollPane gameScrollPane;
 	public void shutdown() {
 		oyun = new AltinAvcisi(sliderWidth.getValue(), sliderHeight.getValue());
-		dialog=new GameDialog(oyun,sliderWidth.getValue(), sliderHeight.getValue());
+		GameDialog dialog=new GameDialog(oyun,sliderWidth.getValue(), sliderHeight.getValue());
 		gameScrollPane = new JScrollPane(oyun);
 		frame.setSize(900, 900);
 		frame.getContentPane().remove(this);
